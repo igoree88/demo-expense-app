543273a478195eb34e87be97b6b3e84911f6f33c
Ihor Yeremenko <igoree86@gmail.com>
2022-07-27 11:47:09 +0300

    Add_components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import { SignUp } from './Components/SingUp';
+import { SignIn } from './Components/SignIn';
+import { Home } from './Components/HomePage';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/auth/register" element={<SignUp />} />
+        <Route path="/auth/login" element={<SignIn />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 };
--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { logOut } from '../../Api/Api';
 
-export const HomePage: React.FC = () => {
+export const Home: React.FC = () => {
+  const navigate = useNavigate();
+  const handlerOnClick = async () => {
+    const response = await logOut();
+
+    if (!response.ok) {
+      throw new Error(`${response.status} - ${response.statusText}`);
+    } else {
+      navigate('/auth/login', { replace: true });
+    }
+  };
+
   return (
     <section className="sign sign--congrat">
       <div className="sign__container sign__container--congrat">
@@ -13,11 +26,13 @@ export const HomePage: React.FC = () => {
           className="sign__congratmainimg"
           src="./images/congrat.svg"
         />
-
         <p className="sign__congrattext">Now you are on the main page. Soon we will provide you with detailed feedback on the result of your work</p>
         <button
           type="button"
           className="sign__button sign__button--congrat"
+          onClick={() => {
+            handlerOnClick();
+          }}
         >
           <span className="sign__buttonname">See you</span>
         </button>
@@ -26,6 +41,7 @@ export const HomePage: React.FC = () => {
           className="sign__congratpeopleimg"
           src="./images/people.svg"
         />
+
       </div>
     </section>
   );
